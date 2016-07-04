8dd77acce656c8491cac15a9196ca411a1d9e7ee
Martin Pretorius <glasnoster@gmail.com>
2016-07-04 21:35:49 +0200

    Add package details

--- mix.exs
+++ mix.exs
@@ -17,15 +17,14 @@ defmodule Eliver.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  def package do
+    [
+      maintainers: ["Martin Pretorius"],
+      licenses:    ["MIT"],
+      links:       %{"GitHub" => "https://github.com/glasnoster/eliver"}
+    ]
+  end
+
   defp deps do
     []
   end
