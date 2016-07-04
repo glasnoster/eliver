da1ed51b0fdfd55118522f7b30cc9b48c9d5f991
Martin Pretorius <glasnoster@gmail.com>
2016-07-04 21:43:31 +0200

    Fix package info

--- mix.exs
+++ mix.exs
@@ -2,23 +2,23 @@ defmodule Eliver.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :eliver,
-     version: "1.0.4",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: "Interactive semantic versioning for Elixir packages",
-     deps: deps()]
+    [
+      app: :eliver,
+      version: "1.0.4",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: "Interactive semantic versioning for Elixir packages",
+      deps: deps(),
+      package: package()
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  def package do
+  defp package do
     [
       maintainers: ["Martin Pretorius"],
       licenses:    ["MIT"],
