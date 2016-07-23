ec56aff116bb5a882ebc3fd754d463e57960422c
Martin Pretorius <glasnoster@gmail.com>
2016-07-23 12:52:41 +0200

    Version 1.2.0:
    * 4
    * 4
    * 4
    * 4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Eliver.Mixfile do
   def project do
     [
       app: :eliver,
-      version: "1.1.2",
+      version: "1.2.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
